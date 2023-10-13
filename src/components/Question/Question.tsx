8ed89b262de772264a6fd0eaf4065211a99344ca
RevanToma <tamrevan@gmail.com>
2023-10-13 12:37:16 +0200

    s

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { FC, useState, useEffect } from "react";
 import { QuestionProps } from "../../types";
 import Button from "../common/Button/Button";
